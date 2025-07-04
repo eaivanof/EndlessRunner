e435a2091e73ac025191205d3be43b4c053c982f
Wangzhiyuan <344182022@qq.com>
2025-07-05 01:39:34 +0700

    Update game.cpp

--- code/game.cpp
+++ code/game.cpp
@@ -1,22 +1,46 @@
 #include "..\head\game.h"
+#include "..\head\BitMap.h"
+#include <windows.h>
 
 Game::Game() {
-    pScreenMem = new DWORD[SCREEN_SIZE]; 
+    pScreenMem = new DWORD[SCREEN_SIZE];
+    memset(pScreenMem, 0, sizeof(DWORD) * SCREEN_SIZE);
 }
 
 Game::~Game() {
-    delete[] pScreenMem;
-    pScreenMem = nullptr;
+    if (pScreenMem) {
+        delete[] pScreenMem;
+        pScreenMem = nullptr;
+    }
 }
 
 void Game::init() {
+    BitMap bmp;
+    bmp.loadBitMap("img\\car.bmp");
 
-} 
+    int w = bmp.getWidth();
+    int h = bmp.getHeight();
+    DWORD* img = bmp.getBuffer();
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            int screenX = 100 + x;
+            int screenY = 100 + y;
+            if (screenX < SCREEN_WIDTH && screenY < SCREEN_HEIGHT) {
+                pScreenMem[screenY * SCREEN_WIDTH + screenX] = img[y * w + x];
+            }
+        }
+    }
+}
 
 void Game::loop() {
-
+    MessageBoxA(nullptr, "The image has been loaded and drawn to the buffer", "Game Loop", MB_OK);
 }
 
 void Game::exit() {
+    MessageBoxA(nullptr, "Game resources have been cleaned up", "Exit", MB_OK);
+}
 
+void Game::drawBufferToScreen() {
+    // TODO
 }
