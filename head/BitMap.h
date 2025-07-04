76cf2ac8a5b16ad4477f35c5662d0b24e8ee2a03
Wangzhiyuan <344182022@qq.com>
2025-07-05 01:29:58 +0700

    Update BitMap.h

--- head/BitMap.h
+++ head/BitMap.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <Windows.h>
+#include <string>
 
 class BitMap {
 public:
-    void loadBitMap();
+    BitMap();
+    ~BitMap();
+    void loadBitMap(const char* filename);
+    DWORD* getBuffer() const;
+    int getWidth() const;
+    int getHeight() const;
+
 protected:
     DWORD* pBitMap = nullptr;
-    int width;
-    int height;
-    int size;
+    int width = 0;
+    int height = 0;
+    int size = 0;
+
     COLORREF RGBcolor(int r, int g, int b);
 };
