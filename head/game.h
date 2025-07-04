509393161c0bde8aa7cd2d43fe5bc5830d9337a9
Wangzhiyuan <344182022@qq.com>
2025-07-05 01:35:46 +0700

    Update game.h

--- head/game.h
+++ head/game.h
@@ -8,8 +8,12 @@ public:
     void init();
     void loop();
     void exit();
+
     DWORD* pScreenMem = nullptr;
     const int SCREEN_WIDTH = 1280;
     const int SCREEN_HEIGHT = 720;
     const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
+
+private:
+    void drawBufferToScreen(); // Отрисовка данных из экранного буфера на реальное окно.
 };
