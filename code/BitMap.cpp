42cbeaea9fa463919e78ea1cf1dfd65ea7943f8b
Wangzhiyuan <344182022@qq.com>
2025-07-08 17:18:27 +0700

    Update BitMap.cpp

--- code/BitMap.cpp
+++ code/BitMap.cpp
@@ -1,88 +1,84 @@
-#include "..\head\BitMap.h"
+#include "../head/BitMap.h"
 #include <cstdio>
 
-BitMap::BitMap() {}
+BitMap::BitMap() : pBitMap(nullptr), width(0), height(0), size(0) {}
 
 BitMap::~BitMap() {
     if (pBitMap) {
         delete[] pBitMap;
-        pBitMap = nullptr;
     }
 }
 
-// Загрузка BMP-файла и сохранение пикселей в буфере pBitMap
+void BitMap::setDimensions(int w, int h) {
+    width = w;
+    height = h;
+    size = width * height;
+}
+
+void BitMap::allocateBuffer() {
+    if (pBitMap) {
+        delete[] pBitMap;
+    }
+    pBitMap = new DWORD[size];
+    memset(pBitMap, 0, size * sizeof(DWORD));
+}
+
 void BitMap::loadBitMap(const char* filename) {
     FILE* file = nullptr;
-
-    // Открываем файл в двоичном режиме
     fopen_s(&file, filename, "rb");
-    if (!file) {
-        MessageBoxA(nullptr, "Unable to open BMP file", filename, MB_OK | MB_ICONERROR);
-        return;
-    }
+    if (!file) return;
 
-    // Читаем заголовок BMP-файла (14 байт)
     BITMAPFILEHEADER fileHeader;
     fread(&fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
-
-    // Проверяем сигнатуру "BM" для подтверждения BMP-файла
-    if (fileHeader.bfType != 0x4D42) {
-        MessageBoxA(nullptr, "Not a valid BMP file", filename, MB_OK | MB_ICONERROR);
+    if (fileHeader.bfType != 0x4D42) { // 'BM'
         fclose(file);
         return;
     }
 
-    // Читаем заголовок информации BMP (40 байт)
     BITMAPINFOHEADER infoHeader;
-    fread(&infoHeader, sizeof(BITMAPINFOHEADER) + 14, 1, file);
+    fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, file);
+    if (infoHeader.biBitCount != 32) {
+        fclose(file);
+        return; // 只支持 32 位图像
+    }
 
-    // Получаем ширину и высоту изображения
     width = infoHeader.biWidth;
     height = infoHeader.biHeight;
     size = width * height;
 
-    // Освобождаем старые данные, если они есть
     if (pBitMap) delete[] pBitMap;
-
-    // Выделяем память под пиксели (по одному DWORD на пиксель)
     pBitMap = new DWORD[size];
 
-    // Вычисляем выравнивание (padding), чтобы длина строки была кратна 4 байтам
-    int padding = (4 - (width * 3) % 4) % 4;
-    BYTE rgb[4]; // Временный массив для хранения BGR-значений одного пикселя
+    fseek(file, fileHeader.bfOffBits, SEEK_SET);
+    BYTE pixel[4];
 
-    // Читаем данные построчно снизу вверх, так как BMP хранит строки в обратном порядке
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            // Считываем 3 байта на пиксель: синий, зелёный, красный
-            fread(rgb, sizeof(BYTE), 4, file);
-
-            // Вычисляем индекс в буфере (с инверсией по вертикали)
-            int index = (height - y - 1) * width + x;
-
-            // Преобразуем BGR в DWORD формат и записываем в буфер
-            pBitMap[index] = RGBcolor(rgb[1], rgb[0], rgb[3], rgb[2]);
+            fread(pixel, 1, 4, file); // BGRA
+            int index = (height - 1 - y) * width + x;
+            pBitMap[index] = RGBcolor(pixel[3], pixel[2], pixel[1], pixel[0]);
         }
-        fseek(file, padding, SEEK_CUR);
     }
 
     fclose(file);
 }
 
-// Преобразует компоненты R, G, B в COLORREF (DWORD) цвет
-COLORREF BitMap::RGBcolor(BYTE a, BYTE r, BYTE g, BYTE b)
-{
-    return (COLORREF)((b) | (((DWORD)(g)) << 8) | (((DWORD)(r)) << 16) | (((DWORD)(a)) << 24));
+void BitMap::drawXyBitMap(HDC hdc, int x, int y) {
+    if (!pBitMap) return;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            DWORD color = pBitMap[i * width + j];
+            BYTE a = (color >> 24) & 0xFF;
+            BYTE r = (color >> 16) & 0xFF;
+            BYTE g = (color >> 8) & 0xFF;
+            BYTE b = color & 0xFF;
+
+            if (a > 0) {
+                SetPixel(hdc, x + j, y + i, RGB(r, g, b));
+            }
+        }
+    }
 }
 
-DWORD* BitMap::getBuffer() const { // Возвращает указатель на буфер пикселей
-    return pBitMap;
-}
 
-int BitMap::getWidth() const { // Возвращает ширину изображения в пикселях
-    return width;
-}
-
-int BitMap::getHeight() const { // Возвращает высоту
-    return height;
-}
